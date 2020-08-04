1cc973f31aaab2c8fcf109501d56857bab6de3a4
YunaK97 <63200480+YunaK97@users.noreply.github.com>
2020-08-05 00:00:23 +0900

    11048_bottomup
    bottomup 방식 재귀

---
+++ 11048_bottomup.cpp
@@ -0,0 +1,25 @@
+//bottom_up
+#include<cstdio>
+#include<cstring> //memset ¶«¿¡ ÇÊ¿ä 
+#include<algorithm>
+using namespace std;
+int a[1001][1001];
+int d[1001][1001];
+int n,m;
+int go(int i,int j){
+	if(i>n||j>m)
+		return 0;
+	if(d[i][j]>=0)
+		return d[i][j];
+	d[i][j]=max(go(i+1,j),go(i,j+1))+a[i][j];
+	return d[i][j];
+}
+int main(){
+	freopen("11048.txt","r",stdin);
+	scanf("%d %d",&n,&m);
+	for(int i=1;i<=n;i++)
+		for(int j=1;j<=m;j++)
+			scanf("%d",&a[i][j]);
+	memset(d,-1,sizeof(d));
+	printf("%d",go(1,1));
+}
